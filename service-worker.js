6b38628748126824fc57453682974241d4aa915c
Ryan How <ryan@bitworks.com.au>
2017-03-23 22:26:44 +0800

    Automatic Deploy

--- service-worker.js
+++ service-worker.js
@@ -7,9 +7,31 @@
 'use strict';
 importScripts('./build/sw-toolbox.js');
 
+self.version = '0.0.41';
+
 self.toolbox.options.cache = {
-  name: 'eBudget-cache-0.0.40'
+  name: 'eBudget-cache-%BUILD_INFO_VERSION%'
 };
+
+self.addEventListener('install', function(event){
+  console.log('installed! ' + self.version);
+  send_message_to_all_clients(self.version + 'installed');
+});
+
+// Delete old caches
+this.addEventListener('activate', function(event) {
+  console.log('activated! ' + self.version);
+  send_message_to_all_clients(self.version + 'activated');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key === self.toolbox.options.cache.name) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
 
 // pre-cache our key assets
 self.toolbox.precache(
@@ -28,3 +50,27 @@ self.toolbox.router.any('/*', self.toolbox.cacheFirst);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 //self.toolbox.router.default = self.toolbox.networkFirst;
+
+function send_message_to_client(client, msg){
+    return new Promise((resolve, reject) => {
+        var msg_chan = new MessageChannel();
+        msg_chan.port1.onmessage = function(event){
+            if(event.data.error){
+                reject(event.data.error);
+            }else{
+                resolve(event.data);
+            }
+        };
+
+        client.postMessage("SW Says: '"+msg+"'", [msg_chan.port2]);
+    });
+}
+
+
+function send_message_to_all_clients(msg){
+    clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
+        })
+    })
+}
