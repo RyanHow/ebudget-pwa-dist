d85174b0ffdf179c777ece7f7006ec9c5251c587
Ryan How <ryan@bitworks.com.au>
2017-03-24 21:44:15 +0800

    Automatic Deploy

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 'use strict';
 importScripts('./build/sw-toolbox.js');
 
-self.version = '0.0.66';
+self.version = '0.0.67';
 
 self.toolbox.options.cache = {
   name: 'eBudget-cache-' + self.version
